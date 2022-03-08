cbabc18210515138ac9c4b6c3b37576cf7e2860c
cocoy02 <ylycoco0022@gmail.com>
2022-03-08 11:41:47 +0800

    March 8

---
+++ bt3103_group2/src/components/borrower.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="filter">
+    <filter1 />
+
+    <filter2 />
+
+    <filter3 />
+  </div>
+</template>
+
+<script>
+import filter1 from "./filter1.vue";
+import filter2 from "./filter2.vue";
+import filter3 from "./filter3.vue";
+export default {
+  name: "borrower",
+  data() {
+    return {
+      checkedNames: [],
+    };
+  },
+  components: {
+    filter1,
+    filter2,
+    filter3,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ bt3103_group2/src/components/lender.vue
@@ -0,0 +1,26 @@
+<template>
+  <filter1 />
+  <filter2 />
+  <filter4 />
+</template>
+
+<script>
+import filter1 from "./filter1.vue";
+import filter2 from "./filter2.vue";
+import filter4 from "./filter4.vue";
+export default {
+  name: "borrower",
+  data() {
+    return {
+      checkedNames: [],
+    };
+  },
+  components: {
+    filter1,filter2,filter4
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
