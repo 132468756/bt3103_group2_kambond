9ca164cfbaf56e4b4cf333326bc6f91765dd80b3
iriszhuhao <e0492471@u.nus.edu>
2022-03-13 10:08:30 +0800

    Mar 12 home page search box

--- bt3103_group2/src/components/Home/SearchField.vue
+++ bt3103_group2/src/components/Home/SearchField.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="container">
-      <br>
+  <div class="search-area">
       <h3 id='searchtitle'> KamBond Search </h3>
-      <div class="inputbox">
-          <br>
-          <input type="text" id="search" required="" Placeholder="Search">
-          
-          <div class="search">
-            <button id="searchbutton" type="button" v-on:click="search()">Search</button>
-          </div>
-          <br><br>
+      <div class="icon">
+        <img src="@/assets/search.jpg" width="" height="">
+      </div>
+      <div class="search-bar">
+        <div class="inputbox">
+          <input type="text" id="search" v-model="searchText" required="" Placeholder="Search">
+        </div>
+        <div class="button">
+          <button id="searchbutton" type="button" v-on:click="search()">Search</button>
+        </div>
       </div>
   </div>
+
 </template>
 
 <script>
@@ -29,7 +31,7 @@ methods: {
 </script>
 
 <style>
-  .container {
+  .search-area {
     /* The image used */
     background-image: url('~@/assets/search-bg.jpg');
 
@@ -44,6 +46,44 @@ methods: {
   }
 
   #searchtitle {
-    color: floralwhite;
+    color: floralwhite; /* floralwhite */
+    text-align: center;
+  }
+
+  .icon {
+    width: 100%;
+    /* background-color: aquamarine; */
+    text-align: center;
+  }
+
+  .search-bar {
+    display:inline-block;
+    text-align: center;
+    width:100%;
+  }
+
+  .inputbox {
+    text-align: center;
+    /* background-color: aqua; */
+    padding: 5%;
+  }
+  #search {
+    width: 80%;
+    height:30px;
+  }
+
+  .button {
+    /* background-color: blue; */
+    text-align: justify;
+    padding:5%;
+  }
+
+  #searchbutton {
+    background-color: floralwhite;
+    color: black;
+    height: 100%;
+  }
+
+  .button:hover {
   }
 </style>
---
+++ bt3103_group2/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIpw5m27hVCzO-OLtwZTM_hMHcX5jHoQc",
+  authDomain: "demofiredev.firebaseapp.com",
+  projectId: "demofiredev",
+  storageBucket: "demofiredev.appspot.com",
+  messagingSenderId: "75003976660",
+  appId: "1:75003976660:web:142737505cbd375a88d441"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseApp;
