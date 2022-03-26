f7db447e9a6a825e04e5acb91eb000656cd3acef
cocoy02 <ylycoco0022@gmail.com>
2022-03-26 20:01:02 +0800

    Update Homepage.vue

--- bt3103_group2/src/views/Homepage.vue
+++ bt3103_group2/src/views/Homepage.vue
@@ -11,10 +11,16 @@ import SearchField from "../components/Home/SearchField.vue";
 import NavBar from "../components/NavBar.vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import firebaseApp from "../firebase.js";
-import { getFirestore, setDoc, doc, getDocs, collection } from "firebase/firestore";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+  getDocs,
+  collection,
+} from "firebase/firestore";
 
 const db = getFirestore(firebaseApp);
-
+const auth = getAuth();
 
 export default {
   name: "Homepage",
@@ -25,7 +31,7 @@ export default {
 
   data() {
     return {
-      user:'',
+      user: "",
     };
   },
 
@@ -33,44 +39,45 @@ export default {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         this.user = user.email;
-        console.log(user.email)
-        console.log(this.user)
+        console.log(user.email);
+        console.log(this.user);
       }
     });
 
     async function createUser(self) {
       try {
         let data = {
-          username:"this is random name",
-          email:self.user,
-          bio:"this is description",
-          contactNumber:0,
-          creditPoint:0,
-          telegramHandle:'',
-          likes:0,
+          username: "this is random name",
+          email: self.user,
+          bio: "this is description",
+          contactNumber: 0,
+          creditPoint: 0,
+          telegramHandle: "",
+          likes: 0,
           posts: [],
           deals: [],
           requests: [],
         };
 
-        let userExits = false
-        let userInfo = await getDocs(collection(db, "Users"))
+        let userExits = false;
+        let userInfo = await getDocs(collection(db, "Users"));
         userInfo.forEach((doc) => {
           // console.log(doc.id, " => ", doc.data());
-          if(doc.id == self.user){
-            userExits=true
+          if (doc.id == self.user) {
+            userExits = true;
           }
         });
-        if(!userExits){// Create user only if this is a new user
-          const docNow = await setDoc(
-            doc(db, "Users", self.user),
-            data
-          );
+        if (!userExits) {
+          // Create user only if this is a new user
+          const docNow = await setDoc(doc(db, "Users", self.user), data);
           console.log(docNow);
         }
-      // } catch (error) {
-      //   console.error("Error adding document:", error);
-      // }
+        // } catch (error) {
+        //   console.error("Error adding document:", error);
+        // }
+      } catch (error) {
+        console.error("Error adding document:", error);
+      }
     }
     createUser(this);
   },
