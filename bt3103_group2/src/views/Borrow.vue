b964b82542cc7954764efa79c20ca6a53c828d7b
Cattttw <e0492489@u.nus.edu>
2022-03-28 22:37:59 +0800

    single choice filter

--- bt3103_group2/src/views/Borrow.vue
+++ bt3103_group2/src/views/Borrow.vue
@@ -1,22 +1,71 @@
 <template>
-<div style="text-align:center;" v-if="user">
+<div style="text-align:center;" v-if="user"> 
   <NavBar/>
-  <div id="filter">
-    <Filter1 />
+  <backBtn/>
+  <form @submit.prevent="onSubmit" method="post" id="createpostform">
 
-    <Filter2 />
+    <div className ="row">
+    <label className = "postlabel"> Status </label>
+    <select required v-model="post.status" id = "post.status">
+      <option>Wants to lend</option>
+      <option>Requested</option>
+      <option>Completed</option>
+    </select>
+    </div>
 
-    <Filter3 />
+    <div className ="row">
+    <label className = "postlabel"> Category </label>
+    <select required v-model="post.category" id = "post.category">
+      <option>Beauty & Personal Care</option>
+      <option>Bulletin Board</option>
+      <option>Computers & Tech</option>
+      <option>Food & Drinks</option>
+      <option>Health & Nutrition</option>
+      <option>Hobbies & Toys</option>
+      <option>Mobile Phones & Gadgets</option>
+      <option>Photography</option>
+      <option>Sports Equipment</option>
+      <option>Tickets & Vouchers</option>
+      <option>Learning & Enrichment</option>
+      <option>Video Gaming</option>
+      <option>Fashion</option>
+      <option>Exchange Currency</option>
+      <option>Musical Instrument</option>
+      <option>Others</option>
+    </select>
+    </div>
 
-  </div>
+    <div className ="row">
+    <label className = "postlabel"> Location </label>
+    <select required v-model="post.location" id = "post.location">
+      <option>PGP / PGPR</option>
+      <option>Utown</option>
+      <option>RVRC</option>
+      <option>Temasek Hall</option>
+      <option>Sheares Hall</option>
+      <option>KEVII Hall</option>
+      <option>Eusoff Hall</option>
+      <option>Raffles Hall</option>
+      <option>Kent Ridge Hall</option>
+      <option>Others</option>
+    </select>
+    </div>
+    <div className = "showRow">
+      <button className="show" @click = "showpost()"> Show </button>
+    </div>
+    
+  
+  </form>
+</div>
   <Modal
         v-show="isModalVisible"
         @close="closeModal"
         :post = "modalData"
       />
   <div className = "postList" 
-    v-for= "post in posts"
-    :key = "post.id">
+    v-for= "post in filteredProducts"
+    :key = "post.id"
+    v-show ="showdata">>
     <button type="button"
           id = "postModal"
           @click="showModal(post)">
@@ -25,60 +74,59 @@
       :title = "post.title"
       :status = "post.status"/>
       
-    </button>
-    
-      
-    
+    </button> 
   </div>
-  <div>
-    
-  </div>
-</div>
+  
 </template>
 
 <script>
-import Filter1 from "@/components/Filter/Filter1.vue";
-import Filter2 from "@/components/Filter/Filter2.vue";
-import Filter3 from "@/components/Filter/Filter3.vue";
+import firebaseApp from "../firebase.js";
+import { getFirestore} from "firebase/firestore";
+import { doc} from "firebase/firestore";
 import Post from "@/components/Post.vue"
 import NavBar from "../components/NavBar.vue"
 import Modal from "../components/Modal.vue"
-import { getAuth, onAuthStateChanged } from "firebase/auth"
-
-import firebaseApp from "@/firebase.js";
-import {getFirestore} from "firebase/firestore";
-import {collection, getDocs, getDoc, doc} from "firebase/firestore";
+import backBtn from "../components/profile/BackButton.vue"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {collection, getDocs, getDoc} from "firebase/firestore";
 const db = getFirestore(firebaseApp);
 
-
 export default {
-  name: "Borrow",
+  name: "CreatePost",
+  components:{
+    NavBar,
+    backBtn,
+    Post,
+    Modal,
+  },
   data() {
     return {
       user: false,
-      checkedNames: [],
+      post:{
+      title: "",
+      purpose: "",
+      desription: "",
+      category: "",
+      location: "",
+      },
       isModalVisible:false,
+      showdata: false,
       modalData:{},
       posts:[],
+      filteredProducts:[],
+      selectedstatus: "",
+      selectedcategory: "",
+      selectedlocation: "",
     };
   },
-  components: {
-    Filter1,
-    Filter2,
-    Filter3,
-    Post,
-    NavBar,
-    Modal,
-  },
-  mounted(){
+  mounted() {
     const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                this.user = user;
-            }
-        })
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          this.user = user;
+        }
+      })
 
-        
     async function collectData(posts){
       let z = await getDocs(collection(db,"Posts"))
       z.forEach((doc)=> 
@@ -86,21 +134,6 @@ export default {
       console.log(posts)
       let docRef = await getDoc(doc(db, "Users", "12345"));
       console.log(docRef.data().username);
-      //computed: {
-      //filteredpost: function() {
-      // var vm = this;
-
-      // checkedNames.forEach((fil) => 
-      //  {
-      //    var category = fil;
-      //    return vm.posts.filter(function(post) {
-      //    return post.category == category;
-      //    });
-      //  }
-      // )
-      //}
-    //}
-
       posts.forEach(async (post)=>{
         docRef = await getDoc(doc(db, "Users", post.user));
         console.log(docRef.data().username)
@@ -111,24 +144,29 @@ export default {
     }
     collectData(this.posts)
 
-    
   },
-  methods:{
-     showModal(data) {
+methods: {
+   
+  showModal(data) {
         this.isModalVisible = true;
         this.modalData = data;
         console.log("isopen")
-      },
+  },
+  showpost() {
+    this.selectedcategory = document.getElementById("post.category").value;
+    this.selectedstatus = document.getElementById("post.status").value;
+    this.selectedlocation = document.getElementById("post.location").value;
+    this.filteredProducts = this.posts.filter(post => post.purpose === "Lending").filter(post =>  post.category === this.selectedcategory).filter(post => post.location = this.selectedlocation).filter(post => post.status === this.selectedstatus);
+    this.showdata = true;  
+  },
   closeModal() {
         this.isModalVisible = false;
-      },
-      
-  }
-};
+ }
 
+},
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #postModal{
   justify-content:center;
@@ -137,6 +175,65 @@ export default {
 .postList{
   display:inline-block;
 }
-
-
+#createpostform {
+  background: rgb(205, 243, 213);
+  padding-bottom: 80px;
+  border-radius: 10px;
+  justify-content:center;
+}
+.postlabel {
+  color: rgb(31, 34, 34);
+  display: flex;
+  justify-content:left;
+  margin-top:2%;
+  margin-bottom:2%;
+  width:100%;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,select {
+  display: flex;
+  justify-content:center;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+.row{
+    display:flex;
+    flex-direction:column;
+    width:40%;
+    margin-left:30%;
+}
+.submitRow{
+    margin-left:60%;
+    padding:1%;
+}
+.submit {
+    position:absolute;
+    text-align: center;
+    background-color: blue;
+    border:0;
+    padding:10px 20px;
+    margin-top:20px;
+    color:aliceblue;
+    border-radius: 20px;
+    width:10%;
+    height:7%;
+    cursor: pointer;
+}
+.submit:hover{
+  outline-color: transparent;
+  outline-style: solid;
+  box-shadow: 0 0 0 1px lightblue;
+  transition: 0.5s;
+}
+.submit:active{
+  background-color: lightblue;
+}
 </style>
+
+
