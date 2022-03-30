00a92560393d0c785057879f81375100d9ac7fa7
cocoy02 <ylycoco0022@gmail.com>
2022-03-30 23:44:46 +0800

    Mar30 Chats

--- bt3103_group2/src/components/profile/ProfileSideBar.vue
+++ bt3103_group2/src/components/profile/ProfileSideBar.vue
@@ -35,6 +35,7 @@
         <MyDealTable id="MyDeals" v-if="this.showDeals" />
         <UserInfoDisplay id="userInfo" v-if="this.showProfile" />
         <CreatePost id="createpostform" v-if="this.showCreate" />
+        <Chats id="chats" v-if="this.showChats" />
         <LogOut v-if="this.showProfile"/>
     </div>
 </div>
@@ -52,6 +53,7 @@ import MyRequestTable from "../profile/MyRequestsTable.vue"
 import MyDealTable from "../profile/MyDealsTable.vue"
 import UserInfoDisplay from "../profile/UserInfoDisplay.vue"
 import CreatePost from "../profile/CreatePostForm.vue"
+import Chats from '../../views/Chats.vue'
 import LogOut from "../Logout.vue"
 
 const db = getFirestore(firebaseApp)
@@ -67,7 +69,8 @@ export default {
         MyDealTable,
         UserInfoDisplay,
         CreatePost,
-        LogOut
+        LogOut,
+        Chats
     },
     data(){
         return{
--- bt3103_group2/src/views/Chats.vue
+++ bt3103_group2/src/views/Chats.vue
@@ -1,9 +1,8 @@
 <template>
-<h1> Chat </h1>
-  <!-- <div v-if="fetched" class="app">
-    <div class="app__body"> -->
-      <!-- <ChatSidebar :rooms="rooms"/> -->
-      <!-- <ChatView :room="firstRoom" />
+  <div v-if="fetched" class="app">
+    <div class="app__body">
+      <ChatSideBar :rooms="rooms"/>
+      <ChatView :room="firstRoom" />
     </div>
   </div>
   <div v-else>
@@ -12,85 +11,77 @@
 </template>
 
 <script>
+import {ref} from 'vue'
+import ChatView from "../components/Chat/ChatView.vue";
+import ChatSideBar from "../components/Chat/ChatSideBar.vue";
+// import ListFriends from "../components/Chat/ListFriends.vue";
+// import ListUsers from "../components/Chat/ListUsers.vue";
+import firebaseApp from "../firebase.js";
+import {
+  getFirestore,
+  getDoc,
+  doc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const db = getFirestore(firebaseApp);
+const auth = getAuth();
 export default {
   name: "Chat",
-}
-// import ChatView from "../components/Chat/ChatView.vue";
-// //import ChatSideBar from "../components/Chat/ChatSideBar.vue";
-// // import ListFriends from "../components/Chat/ListFriends.vue";
-// // import ListUsers from "../components/Chat/ListUsers.vue";
-// import firebaseApp from "../firebase.js";
-// import {
-//   getFirestore,
-//   getDoc,
-//   doc,
-//   collection,
-//   getDocs,
-// } from "firebase/firestore";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// const db = getFirestore(firebaseApp);
-// const auth = getAuth();
-// export default {
-//   name: "Chat",
-//   //components: { ChatSideBar, ChatView, ListFriends, ListUsers },
-//   components: { ChatView },
-//   data() {
-//     return {
-//       fetched: false,
-//       friends: [],
-//       email: "",
-//       rooms: [],
-//       firstRoom: {},
-//       username: "",
-//       user: "",
-//     };
-//   },
-//   methods: {
-//     async listFriends() {
-//       const docRef = await getDoc(doc(db, "Users", auth.currentUser.email));
-//       this.friends = docRef.data().chatrooms;
-//       console.log("friends", this.friends);
-//     },
-//     async getRooms() {
-//       this.fetched = true;
-//       const docNow = await getDocs(collection(db, "Chats"));
+  //components: { ChatSideBar, ChatView, ListFriends, ListUsers },
+  components: { ChatView, ChatSideBar},
+  data() {
+    return {
+      fetched: false,
+      friends: [],
+      rooms: [],
+      firstRoom: {},
+      user: "",
+    };
+  },
+  methods: {
+    async listFriends() {
+      const docRef = await getDoc(doc(db, "Users", auth.currentUser.email));
+      this.friends = docRef.data().chatrooms;
+      console.log("friends", this.friends);
+    },
+    async getRooms() {
+      this.fetched = true;
+      const docNow = await getDocs(collection(db, "Chats"));
       
-//       docNow.forEach((doc) => {
-//         console.log(doc.data().user2 == String(auth.currentUser.displayName));
-//         if (
-//           (doc.data().user1 == String(auth.currentUser.email)) |
-//           (doc.data().user2 == String(auth.currentUser.email))
-//         ) {
-//           this.rooms.push(doc);
-//         }
-//       });
-//       console.log("rooms", this.rooms);
-//       if (this.rooms.length > 0) {
-//         console.log("update chatview by first room", this.rooms[0]);
-//         this.firstRoom = this.rooms[0];
-//       }
-//     },
-//   },
-//   mounted() {
-//     // if (!this.username) {
-//     //   this.$router.push("/");
-//     // } else {
-//     //   this.getRooms();
-//     // }
-//     onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         this.user = user.email;
-//         console.log(user.email);
-//         console.log(this.user);
-//         this.getRooms();
-//       }
-//     });
-//   },
-// };
+      docNow.forEach((doc) => {
+        console.log(doc.data().user2 == String(auth.currentUser.displayName));
+        if (
+          (doc.data().user1 == String(auth.currentUser.email)) |
+          (doc.data().user2 == String(auth.currentUser.email))
+        ) {
+          this.rooms.push(doc.data().chatRoomName);
+        }
+      });
+      console.log("rooms", this.rooms);
+      console.log(ref(this.rooms))
+      if (this.rooms.length > 0) {
+        console.log("update chatview by first room", this.rooms[0]);
+        this.firstRoom = this.rooms[0];
+      }
+    },
+  },
+  mounted() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user.email;
+        console.log(user.email);
+        console.log(this.user);
+        this.getRooms();
+      }
+    });
+  },
+};
 </script>
 
 <style scoped>
-/* .app {
+.app {
   display: grid;
   place-items: center;
   background-color: #dadbd3;
@@ -107,5 +98,5 @@ export default {
 
 .md-progress-spinner {
   place-self: center;
-} */
+}
 </style>
