bba3dbe7ff47a2c5c80b76dac40b234977018c47
Cattttw <e0492489@u.nus.edu>
2022-04-06 12:20:53 +0800

    debug

--- bt3103_group2/src/components/Modal.vue
+++ bt3103_group2/src/components/Modal.vue
@@ -69,6 +69,7 @@
               <button @click = "toBorrow(this)"
               class = "borrowButton"> Lend </button>
             </div>
+            </div>
         </footer>
       </div>
     </div> <!-- modal-backgrop -->
