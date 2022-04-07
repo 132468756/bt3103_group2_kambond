e2ff039843b460362217e409fcd223c8a5854e8f
nuoyiJ <jiangnuoyi1999@gmail.com>
2022-04-07 17:02:56 +0800

    nav bar have logo

--- bt3103_group2/src/components/NavBar.vue
+++ bt3103_group2/src/components/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="nav">
+      <img src = "@/assets/logo.png" id= "navImg"/>
       <router-link to="/home">Home</router-link> &nbsp; | &nbsp;
       <router-link to="/borrow">Borrow</router-link> &nbsp; | &nbsp;
       <router-link to="/lend">Lend</router-link> &nbsp; | &nbsp;
@@ -27,5 +28,13 @@ export default {
 }
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#navImg {
+  height: 13vh;
+  width: 9vw;
+  position:absolute;
+  left: 1vw;
+  top: 2vh;
 }
 </style>
